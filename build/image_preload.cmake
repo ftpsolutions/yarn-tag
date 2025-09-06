39836c23175d105dd767bd026b75f707556c1866
Acea Quigg <acea.quigg@ftpsolutions.com.au>
2025-09-06 20:55:54 +1000

    cleaning house

--- build/image_preload.cmake
+++
@@ -1,6 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD "nrf52dk_nrf52810" CACHE INTERNAL "NCS child image controlled")
-set(CACHED_CONF_FILE "/Users/acea/Documents/FTP/software/yarn-tag/prj.conf" CACHE INTERNAL "NCS child image controlled")
-set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "/Users/acea/Documents/FTP/software/yarn-tag/boards/nrf52dk_nrf52810.overlay" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "/opt/nordic/ncs/toolchains/20d68df7e5/opt/python@3.9/bin/python3.9" CACHE INTERNAL "NCS child image controlled")
